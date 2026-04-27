6bb8000472f1f96bb58a6f853a0248ae7c43f576
Kimmoongi0320 <moongi0320@naver.com>
2026-04-27 16:39:01 +0900

    [#9] feat: DB 연결 및 종속성 초기화

--- backend/src/main/java/com/github/logi/LogiApplication.java
+++ backend/src/main/java/com/github/logi/LogiApplication.java
@@ -2,9 +2,8 @@ package com.github.logi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class LogiApplication {
 
 	public static void main(String[] args) {
